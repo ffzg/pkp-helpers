dcd4a36204876516805659c9edcb233b31322f13
Dobrica Pavlinusic <dpavlin@rot13.org>
2021-05-11 14:57:31 +0200

    support ojs and omp upgrade with same script

--- ojs-upgrade.sh
+++ ojs-upgrade.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -xe
 
-dir=/var/www/ojs
+app=$( basename $0 | cut -d- -f1 )
+dir=/var/www/$app
 php=/usr/bin/php7.3
 database_name=$( grep -A 100 '^\[database\]' $dir/config.inc.php  | grep ^name | head -1 | cut -d= -f2 )
 
 date=$( date +%Y-%m-%d )
 
-test -z "$1" && echo "Usage: $0 ~/ojs-3.3.0-6.tar.gz" && exit 1
+test -z "$1" && echo "Usage: $0 ~/$app-3.3.0-6.tar.gz" && exit 1
 test "$( id -u )" -ne 0 && echo "re-run as root: sudo $0 $*" && exit 1
 
 targz=$1
@@ -15,9 +16,9 @@ tar_dir=$( basename $targz | sed 's/.tar.gz//')
 
 
 echo "Backup $dir and $database_name"
-tar cfpz /tmp/ojs-backup-$date.tar.gz $dir /srv/ojs/
-mysqldump $database_name | gzip > /tmp/ojs-backup-$date.sql.gz
-ls -al /tmp/ojs-backup-$date*
+tar cfpz /tmp/$app-backup-$date.tar.gz $dir /srv/$app/
+mysqldump $database_name | gzip > /tmp/$app-backup-$date.sql.gz
+ls -al /tmp/$app-backup-$date*
 
 echo "Upgrade to $tar_dir"
 
@@ -27,17 +28,17 @@ tar xf $targz
 chown -R root:root $tar_dir
 chown -R www-data $tar_dir/cache/ $tar_dir/public/
 
-cp -vp ojs/config.inc.php $tar_dir
-cp -rvp ojs/public/* $tar_dir/public/
+cp -vp $app/config.inc.php $tar_dir
+cp -rvp $app/public/* $tar_dir/public/
 
-cp -rpv ojs/plugins/generic/defaultTranslation $tar_dir/plugins/generic/
+cp -rpv $app/plugins/generic/defaultTranslation $tar_dir/plugins/generic/
 
-mv ojs ojs.old.$date
-mv $tar_dir ojs
+mv $app $app.old.$date
+mv $tar_dir $app
 
-cd ojs
+cd $app
 
 sudo -u www-data $php tools/upgrade.php check
 
-sudo -u www-data $php tools/upgrade.php upgrade 2>&1 | tee ~/ojs-upgrade-$date.log
+sudo -u www-data $php tools/upgrade.php upgrade 2>&1 | tee ~/$app-upgrade-$date.log
 
